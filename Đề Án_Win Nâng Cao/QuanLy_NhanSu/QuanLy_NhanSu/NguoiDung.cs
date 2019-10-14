476fc8466e33be846951df0d661533ac466dca28
asxi1998 <44825957+asxi1998@users.noreply.github.com>
2019-10-14 17:52:43 +0700

    linQ first test

---
+++ Đề Án_Win Nâng Cao/QuanLy_NhanSu/QuanLy_NhanSu/NguoiDung.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLy_NhanSu
+{
+    public static class NguoiDung
+    {
+        public static List<string> GetMaNhomNguoiDung(string maNguoiDung)
+        {
+            List<string> list = new List<string>();
+
+            //SqlDataAdapter daUser = new SqlDataAdapter("", Properties.Settings.Default.DA_WIN_NANGCAOConnectionString);
+            //DataTable dt = new DataTable();
+            //daUser.Fill(dt);
+            ////if (dt.Rows.Count == 0)
+            return list;
+        }
+        public static DataTable GetMaManHinh(string item)
+        {
+            DataTable list = new DataTable();
+
+
+            return list;
+        }
+    }
+}
--- Đề Án_Win Nâng Cao/QuanLy_NhanSu/QuanLy_NhanSu/frm_Main.cs
+++ Đề Án_Win Nâng Cao/QuanLy_NhanSu/QuanLy_NhanSu/frm_Main.cs
@@ -12,6 +12,7 @@ namespace QuanLy_NhanSu
 {
     public partial class frm_Main : Form
     {
+        
         public frm_Main()
         {
             InitializeComponent();
@@ -39,7 +40,61 @@ namespace QuanLy_NhanSu
 
         private void frm_Main_Load(object sender, EventArgs e)
         {
+            LinQDataContext linQDataContext = new LinQDataContext();
 
+            DataGridView nhomND = new DataGridView();
+
+            var list =from danhSachNhom in linQDataContext.NGUOIDUNGNHOMNGUOIDUNGs
+                       join taikhoan in linQDataContext.TAIKHOANs
+                       on danhSachNhom.MANV equals taikhoan.MANV
+                       where taikhoan.MANV == Properties.Settings.Default.user
+                       select new { danhSachNhom.MANHOM };
+
+            nhomND.DataSource=list;
+            //foreach (string item in nhomND.DataSource)
+            //{
+            //    DataTable dsQuyen = NguoiDung.GetMaManHinh(item);
+            //    foreach (DataRow mh in dsQuyen.Rows)
+            //    {
+            //        //FindMenuPhanQuyen(this.menuStrip1.Items,mh[0].ToString(), Convert.ToBoolean(mh[1].ToString()));
+            //    }
+            //}
         }
+        private void FindMenuPhanQuyen(ToolStripItemCollection mnuItems, string pScreenName, bool pEnable)
+        {
+            foreach (ToolStripItem menu in mnuItems)
+            {
+                if (menu is ToolStripMenuItem &&
+                ((ToolStripMenuItem)(menu)).DropDownItems.Count > 0)
+                {
+                    FindMenuPhanQuyen(((ToolStripMenuItem)(menu)).DropDownItems,
+                    pScreenName, pEnable);
+                    menu.Enabled =
+                    CheckAllMenuChildVisible(((ToolStripMenuItem)(menu)).DropDownItems);
+                    menu.Visible = menu.Enabled;
+                }
+                else if (string.Equals(pScreenName, menu.Tag))
+                {
+                    menu.Enabled = pEnable;
+                    menu.Visible = pEnable;
+                }
+            }
+        }
+        private bool CheckAllMenuChildVisible(ToolStripItemCollection mnuItems)
+        {
+            foreach (ToolStripItem menuItem in mnuItems)
+            {
+                if (menuItem is ToolStripMenuItem && menuItem.Enabled)
+                {
+                    return true;
+                }
+                else if (menuItem is ToolStripSeparator)
+                {
+                    continue;
+                }
+            }
+            return false;
+        }
+
     }
 }
